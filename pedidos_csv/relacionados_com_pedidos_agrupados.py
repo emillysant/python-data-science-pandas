55531dd0b6dac8584ba80e53f92ac35f45d6e0c6
Danilo <danilo.aparecido.santos@gmail.com>
2023-02-07 21:43:00 +0000

    mais relacional dataframe

--- pedidos_csv/relacionados_com_pedidos_agrupados.py
+++ pedidos_csv/relacionados_com_pedidos_agrupados.py
@@ -3,7 +3,6 @@ import pandas as pd
 def risco():
     print("-" * 110)
 
-
 # Lendo os arquivos CSV usando o método read_csv
 clientes_df = pd.read_csv("clientes.csv")
 pedidos_df = pd.read_csv("pedidos.csv")
@@ -16,6 +15,10 @@ risco()
 
 # # Fazendo o merge dos dois DataFrames usando o método merge e especificando o campo cliente_id como chave
 resultado = pedidos_df.merge(clientes_df, how='inner', left_on="cliente_id", right_on="id")
-
-# # Mostrando o resultado
 print(resultado)
+risco()
+
+# Agrupando os dados pelo campo cliente_id e usando o método size para calcular a contagem de linhas
+total_pedidos = resultado.groupby(['nome'])['nome', "valor_total"].mean()
+print(total_pedidos)
+risco()
